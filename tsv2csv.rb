a37d35c5fcd478649243dc4041310994b636f858
Henry Borchers <henryborchers@yahoo.com>
2016-06-09 14:00:12 -0500

    fixed issue with module import

--- tsv2csv.rb
+++ tsv2csv.rb
@@ -5,13 +5,14 @@
 class Tsv2csv < Formula
   desc "Converts a tsv file to a csv file"
   homepage "https://github.com/henryborchers/"
-  url "https://github.com/henryborchers/tsv2csv/archive/tsv2csv_v0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "0fa7cecf4ee4e092015e0f40b60da6937207ca2422cf48b78bb1df79e1091b32"
+  url "https://github.com/henryborchers/tsv2csv/archive/tsv2csv_v0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "5b58c487b5141353b5936e935506226dea667c9e3dee4bac99a4cc20249ef674"
 
   depends_on :python3
 
   def install
+  ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3/site-packages"
 	system "python3", *Language::Python.setup_install_args(prefix)
   end
 end
