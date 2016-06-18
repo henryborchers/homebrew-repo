e0e4388e3ba60914d9df53be3abafbc97725fdaa
Henry Borchers <henryborchers@yahoo.com>
2016-06-18 15:52:01 -0500

    init commit

--- checkinternet.rb
+++ checkinternet.rb
@@ -1,9 +1,10 @@
 class Checkinternet < Formula
   desc "Checks internet once a minute to see the computer has access."
   homepage "https://github.com/henryborchers/checkInternet"
-  url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "19c8ea0f10c26b21efc53baa60946cdeb77af2e0ff8cd087a0a01a793a3898cb"
+  # url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.1.0.tar.gz"
+  version "0.1.0"
+  # sha256 "19c8ea0f10c26b21efc53baa60946cdeb77af2e0ff8cd087a0a01a793a3898cb"
+  sha256 "1d29bca4c25292519324efa731370b6a9788d8d61bbcd32125209f7ad883f12a"
 
   depends_on :python3
   
