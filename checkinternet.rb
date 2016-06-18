e9d8c31bbaaf25dbf24208efd1646deebb1a23e7
henryborchers <henryborchers@yahoo.com>
2016-06-18 16:07:02 -0500

    fixed accidental comment

--- checkinternet.rb
+++ checkinternet.rb
@@ -1,7 +1,7 @@
 class Checkinternet < Formula
   desc "Checks internet once a minute to see the computer has access."
   homepage "https://github.com/henryborchers/checkInternet"
-  # url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.1.0.tar.gz"
+  url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.1.0.tar.gz"
   version "0.1.0"
   # sha256 "19c8ea0f10c26b21efc53baa60946cdeb77af2e0ff8cd087a0a01a793a3898cb"
   sha256 "1d29bca4c25292519324efa731370b6a9788d8d61bbcd32125209f7ad883f12a"
