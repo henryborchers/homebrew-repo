0c128c677b184b526402b52556be09e098a390f2
henryborchers <henryborchers@yahoo.com>
2016-08-30 17:07:49 -0500

    Update checkinternet.rb

--- checkinternet.rb
+++ checkinternet.rb
@@ -1,8 +1,8 @@
 class Checkinternet < Formula
   desc "Checks internet once a minute to see the computer has access."
   homepage "https://github.com/henryborchers/checkInternet"
-  url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.2.4.tar.gz"
-  sha256 "dd308d4eb40ae7223684479fe6bff06b6bc5bd872e66a3ae35a5bd183e84508d"
+  url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.2.5.tar.gz"
+  sha256 "0335f2b3397fe699167e4fe89599d3a86ac05f583c319e792297f1f6052fee12"
 
   depends_on :python3
 
