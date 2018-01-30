b8dea0acd5eeb44f57a2647da34270f915bdc8a9
Henry Borchers <henryborchers@yahoo.com>
2018-01-30 11:05:02 -0600

    use python_3

--- checkinternet.rb
+++ checkinternet.rb
@@ -4,7 +4,7 @@ class Checkinternet < Formula
   url "https://github.com/henryborchers/checkInternet/archive/checkInternet_v0.2.9.tar.gz"
   sha256 "6baa2c005695ead62daf6d569915b7b0c45c8c724cf0a4f7ca3b3502a6ec6431"
 
-  depends_on :python3
+  depends_on :python_3
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3/site-packages"
